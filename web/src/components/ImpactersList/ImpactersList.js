4d4be052c7da3f22f14339c619c92983f543fac3
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2021-03-27 15:28:12 +0100

    npm install axios & fetch impacters list

--- web/src/components/ImpactersList/ImpactersList.js
+++ web/src/components/ImpactersList/ImpactersList.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+import './styles/ImpactersList.css';
+
+const proxy = 'http://localhost:3001';
 
 const ImpactyersList = () => {
+  const [impactersList, setImpactersList] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${proxy}/impacters`)
+      .then((res) => res.data)
+      .then((data) => setImpactersList(data));
+  }, []);
 
-    return (
-        <div>
-            Hello Assignement
-        </div>
-    );
+  console.log(impactersList);
+  return <div>Hello Assignement</div>;
 };
 
 export default ImpactyersList;
