c4c29f32cf06cf68b78f65c7b5a9dbf10784d8dd
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2021-03-27 15:05:58 +0100

    create app structure & first component

---
+++ web/src/components/ImpactersList/ImpactersList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ImpactyersList = () => {
+
+    return (
+        <div>
+            Hello Assignement
+        </div>
+    );
+};
+
+export default ImpactyersList;
