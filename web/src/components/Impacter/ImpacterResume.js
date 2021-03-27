98ce3bc3334b7b39fc05437f6b7e3988e1ef94ca
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2021-03-27 23:01:30 +0100

    fetch & display impacter's datas

--- web/src/components/Impacter/ImpacterResume.js
+++ web/src/components/Impacter/ImpacterResume.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState, useContext} from 'react';
+import axios from 'axios';
+
+import appContext from '../../context/appContext/appContext';
 
 import './styles/ImpacterResume.css';
 
-const ImpacterResume = () => {
-  return <div>impacter data</div>;
+const proxy = 'http://localhost:3001';
+
+const Impacters = () => {
+  const appState = useContext(appContext);
+
+  let a = '';
+  if (appState.state) {
+    if (appState.state.impacterId) {
+      a = appState.state.impacterId;
+    }
+  }
+
+  const [impacter, setImpacter] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`${proxy}/impacters/${a}`)
+      .then((res) => res.data)
+      .then((data) => setImpacter(data));
+  }, [a]);
+
+  return (
+    <div className="impacterResume-container">
+      <div className="impacterResume-name">{impacter.name}</div>
+      <img
+        src={impacter.profile_image}
+        alt="impacter image"
+        className="impacterResume-img"
+      />
+      <div className="impacterResume-bio">{impacter.bio}</div>
+    </div>
+  );
 };
 
-export default ImpacterResume;
+export default Impacters;
