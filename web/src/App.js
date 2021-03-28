ad238766b59d06b8e88283a7ba9029c09582e75f
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2021-03-28 18:17:17 +0100

    add a basic mobile view with menu

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import ImpactersList from './components/ImpactersList/ImpactersList';
 import ImpacterResume from './components/Impacter/ImpacterResume';
 import ImpacterPosts from './components/Impacter/ImpacterPosts';
+import MenuBurger from './components/header/MenuBurger';
 
 import './App.css';
 
+import burgerIcon from './icon/liste.svg';
+import closeIcon from './icon/close.svg';
+
 const App = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="App">
-      <div className="header" />
+      <div className="header">
+        <div className="header-menuBurger" onClick={() => setOpen(!open)}>
+          {open ? (
+            <img
+              src={closeIcon}
+              alt="close menu"
+              className="header-menuBurgerIcon"
+            />
+          ) : (
+            <img
+              src={burgerIcon}
+              alt="menu"
+              className="header-menuBurgerIcon"
+            />
+          )}
+        </div>
+        {open ? (
+          <div className="menuBurger">
+            <MenuBurger open={open} setOpen={setOpen} />
+          </div>
+        ) : null}
+      </div>
       <div className="body">
         <div className="impactersList">
           <ImpactersList />
---
+++ web/src/components/header/MenuBurger.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ImpactersList from '../../components/ImpactersList/ImpactersList';
+
+import './styles/menuBurger.css';
+
+const MenuBurger = () => {
+    return (
+        <div className="menuBurger-container" >
+            <ImpactersList />
+        </div>
+    )
+}
+
+export default MenuBurger;
