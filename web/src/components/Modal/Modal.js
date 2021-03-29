afd3684a1ffa463ac3d4c0d1286b2964480d5207
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2021-03-29 08:49:00 +0200

    npm install react-hook-form & modify a post description feature

--- web/src/components/Modal/Modal.js
+++ web/src/components/Modal/Modal.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useForm} from 'react-hook-form';
+import axios from 'axios';
 
 import './styles/modal.css';
 
+const proxy = 'http://localhost:3001';
+
 const Modal = ({setModal, modal, posts, modalIndex, setModalIndex}) => {
+  const {register, handleSubmit} = useForm();
+  const [modifyWindow, setModifyWindow] = useState(false);
+
+  const modifyPost = (data) => {
+    const {type, description} = data;
+    axios
+      .put(`${proxy}/posts/${posts[modalIndex].id}`, {
+        type,
+        description,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   const hideModal = () => {
     setModal(false);
   };
@@ -57,6 +79,24 @@ const Modal = ({setModal, modal, posts, modalIndex, setModalIndex}) => {
           onClick={() => nextPost(modalIndex)}>
           Next
         </button>
+        <button
+          className="modal-modifyButton"
+          name="modify"
+          type="button"
+          onClick={() => setModifyWindow(true)}>
+          Modify
+        </button>
+        {modifyWindow ? (
+          <form onSubmit={handleSubmit(modifyPost)}>
+            <input name="type" ref={register} placeholder="Type" />
+            <input
+              name="description"
+              ref={register}
+              placeholder="Description"
+            />
+            <input type="submit" value="Modify now!" />
+          </form>
+        ) : null}
       </div>
     </div>
   );
